21d2ccbc7fc22e0aa9d2167eec063a27702e72ee
WyattB2718 <wboying2006@gmail.com>
2024-11-13 21:32:49 -0500

    my first commit

--- whackamole.py
+++ whackamole.py
@@ -1,5 +1,5 @@
 import pygame
-#test
+#testpyyh
 
 def main():
     try:
@@ -15,8 +15,14 @@ def main():
                 if event.type == pygame.QUIT:
                     running = False
             screen.fill("light green")
+            screen.blit(mole_image, mole_image.get_rect(topleft=(0, 0)))
+            for x1 in range (0, 641, 32):
+                pygame.draw.line(screen, "black", (x1, 0), (x1, 512))
+            for y1 in range (0, 513, 32):
+                pygame.draw.line(screen, "black", (0, y1), (640, y1))
             pygame.display.flip()
             clock.tick(60)
+
     finally:
         pygame.quit()
 
